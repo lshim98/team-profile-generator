05a250400b02cb2847d824f8b2672aa8bfd44b53
Lydia <lshim1720@gmail.com>
2021-04-02 16:27:01 -0400

    Create manager-test.js

---
+++ Tests/manager-test.js
@@ -0,0 +1,20 @@
+const Manager = require('../Dev/Manager(s)');
+const Employee = require('../Dev/Work-employees');
+
+
+describe('Manager', () => {
+
+    it('email address entered', () => {
+        const managerEmail = "email@email.com";
+        const result = new managerEmail();
+
+        expect(result).toEqual("email@email.com");
+    });
+
+    it('phone number entered', () => {
+        const managerPhone = "916-475-0812";
+        const result = new managerPhone();
+
+        expect(result).toEqual("916-475-0812");
+    });
+});
