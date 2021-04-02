88022ffe63a63ae74d1083cdaed926753b740363
Lydia <lshim1720@gmail.com>
2021-04-02 16:29:39 -0400

    Create work-employees-test.js

---
+++ Tests/work-employees-test.js
@@ -0,0 +1,19 @@
+const Employee = require('../Dev/work-employees');
+
+
+describe('Employee', () => {
+
+    it('should return an entered name', () => {
+        // const name = "David";
+        // const result = new name();
+
+        expect(name).toEqual("David");
+    });
+
+    it('should return an entered number', () => {
+        // const employeeID = 1234;
+        // const result = new employeeID();
+
+        expect(employeeID).toEqual(1234);
+    });
+});
