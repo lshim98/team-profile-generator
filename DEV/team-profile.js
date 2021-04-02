7e4fc6b78c65e802a0d5ba74b12d6de49d5ff074
Lydia <lshim1720@gmail.com>
2021-04-02 16:13:39 -0400

    Create team-profile.js

---
+++ DEV/team-profile.js
@@ -0,0 +1,45 @@
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+const Manager = require("./Manager");
+
+function generateHTML(data) {
+
+    const generateManager = manager => {
+        return `<div class="cardBody" id="manager">
+        <h2 class="manHeading">${manager.getRole()}</h2>
+        <p><strong>${manager.getName()}</strong></p>
+        <p>${manager.getID()}</p>
+        <p><a href=${manager.getEmail()}>${manager.getEmail()}</a></p>
+        <p>${manager.getPhone()}</p>
+        </div>`
+    }
+
+    const generateEngineer = engineer => {
+        return `<div class="cardBody" id="engineer">
+        <h2 class="engHeading">${engineer.getRole()}</h2>
+        <p><strong>${engineer.getName()}</strong></p>
+        <p>${engineer.getID()}</p>
+        <p><a href="${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+        <p><a href="${engineer.getGithub()}">${engineer.getGithub()}</a></p>
+        </div>`
+    }
+
+    const generateIntern = intern => {
+        return `<div class="cardBody" id="intern">
+        <h2 class="intHeading">${intern.getRole()}</h2>
+        <p><strong>${intern.getName()}</strong></p>
+        <p>${intern.getID()}</p>
+        <p><a href=${intern.getEmail()}>${intern.getEmail()}</a></p>
+        <p><a href=${intern.getGithub()}>${intern.getGithub()}</a></p>
+        <p>${intern.getSchool()}</p>
+        </div>`
+    }
+
+    const html = []
+
+    //push data for each employee into html
+    html.push(data.filter(employee => employee.getRole() === 'Manager').map(manager => generateManager(manager)))
+    html.push(data.filter(employee => employee.getRole() === 'Engineer').map(engineer => generateEngineer(engineer)))
+    html.push(data.filter(employee => employee.getRole() === 'Intern').map(intern => generateIntern(intern)))
+
+module.exports = generateHTML;
