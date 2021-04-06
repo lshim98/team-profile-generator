d5b2d363628d176069a6ab5015ce0f319321e386
Lydia <lshim1720@gmail.com>
2021-04-05 20:22:27 -0400

    Update intern-test.js

--- Tests/intern-test.js
+++ Tests/intern-test.js
@@ -23,3 +23,27 @@ describe('Intern', () => {
         expect(result).toEqual("The OSU Bootcamp");
     });
 });
+
+describe('Intern-#2', () => {
+
+    it('email address entered', () => {
+        const employeeEmail = "intern@email.com";
+        const result = new employeeEmail();
+
+        expect(result).toEqual("intern@email.com");
+    });
+
+    it('github url entered', () => {
+        const github = "github.com/intern-#2";
+        const result = new github();
+
+        expect(result).toEqual("github.com/intern-#2");
+    });
+
+    it('school name entered', () => {
+        const school = "The OSU Bootcamp";
+        const result = new school();
+
+        expect(result).toEqual("The OSU Bootcamp");
+    });
+});
